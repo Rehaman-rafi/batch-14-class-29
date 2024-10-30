596fabe64d733685c251bfd6949aa15f0a633a11
Rehaman-rafi <Rehamanrafi19@gmail.com>
2024-10-30 21:26:34 +0600

    loop & function

---
+++ script.js
@@ -0,0 +1,60 @@
+// let marks = [50,10,20,30];
+
+// """""""""""""for loop"""""""""
+
+// for(i=0; i<marks.length; i++){
+//     console.log(marks[i]);
+// }
+ 
+// =======while loop========
+
+// i = 0;
+// while(i < marks.length){
+//     console.log(marks[i]);
+//     i++;
+// }
+
+// =========DO while========
+
+// let i = 0;
+// do{
+//     console.log(marks[i]);
+//     i++;
+// }
+// while(i < marks.length);
+
+// =====For Each loop=====
+
+// marks.forEach(element=>{
+//     console.log(element);
+// })
+
+// ======Function===============
+
+// function show(){
+//     // console.log("hello world");
+
+//     let marks = [10,20,30,40];
+//     for(i=0; i<marks.length; i++){
+//         console.log(marks[i]);
+//     }
+// }
+// show();
+
+function Sum(number1,number2,number3){
+    //  let x = 10;
+    //  let y = 20;
+    //  let z = 30;
+//      let result = x+y+z;
+//      console.log(result);
+
+// let x = number1;
+// let y = number2;
+// let z = number3;
+// let result = x+y+z;
+
+let result = number1+number2+number3;
+// console.log(result);
+return result;
+}
+console.log(Sum(60,20,50));
